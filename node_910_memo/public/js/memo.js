c8f4e056364db8f890d9e757f81fe2ba61aafb38
pizzajoa <jjh0954@naver.com>
2024-02-16 13:43:32 +0900

    1

---
+++ node_910_memo/public/js/memo.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const date_form = document.querySelector("form.date");
+  const input_form = document.querySelector("form.input");
+  const btn_save = document.querySelector("input.btn_save");
+  const btn_new = document.querySelector("input.btn_new");
+
+  btn_new = document.addEventListener("click", async () => {
+    try {
+      const result = await fetch("/get_new_date");
+      const json = await result.json();
+      date_form.querySelector("input.todate").value = json.toDate;
+      date_form.querySelector("input.totime").value = json.toTime;
+    } catch (error) {
+      alert("서버 통신오류");
+    }
+  });
+
+  btn_save.addEventListener(click, () => {
+    const toDate = date_form.querySelector("imput.todate").value;
+    const toTime = date_form.querySelector("imput.todate").value;
+    const toSubject =
+      input_form.querySelector("imput.tosubject").value;
+    const toMemo = input_form.querySelector("imput.tomemo").value;
+    console.Log(toDate, toTime, toSubject, toMemo);
+  });
+});
